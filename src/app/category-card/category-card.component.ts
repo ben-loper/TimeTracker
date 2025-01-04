102b5858ee868f17df910693a706adad924cffd2
Ben <loperb06@gmail.com>
2025-01-04 00:15:29 -0500

    Add buttons for delete/editing categories

--- src/app/category-card/category-card.component.ts
+++ src/app/category-card/category-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.css'
 })
