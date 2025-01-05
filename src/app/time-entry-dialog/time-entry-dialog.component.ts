087fb8a0be477f14227b619c422641c29112657f
Ben <loperb06@gmail.com>
2025-01-04 23:07:28 -0500

    Add date to the time entry

--- src/app/time-entry-dialog/time-entry-dialog.component.ts
+++ src/app/time-entry-dialog/time-entry-dialog.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CategoryDto } from '../../models/CategoryDto';
 import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { FormsModule } from '@angular/forms';
 import { CategoryService } from '../../services/category.service';
 import { TimeEntryDto } from '../../models/TimeEntryDto';
@@ -15,18 +15,26 @@ import { TimeEntryDto } from '../../models/TimeEntryDto';
     MatDialogActions,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule
   ],
   templateUrl: './time-entry-dialog.component.html',
   styleUrl: './time-entry-dialog.component.css'
 })
-export class TimeEntryDialogComponent {
+export class TimeEntryDialogComponent implements OnInit {
+
   readonly dialogRef = inject(MatDialogRef<TimeEntryDialogComponent>);
   readonly categoryService = inject<CategoryService>(CategoryService);
   readonly data = inject<TimeEntryDialogData>(MAT_DIALOG_DATA);
 
   @Input() hours: number = 0;
   @Input() minutes: number = 0;
+  @Input() date: Date = new Date();
+
+
+  ngOnInit(): void {
+    this.date = new Date();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -36,8 +44,8 @@ export class TimeEntryDialogComponent {
     let entitySaved = false;
     if (!this.data.categoryId) return entitySaved;
 
-    if (this.hours > 0 || this.minutes > 0) {
-      let timeEntry = new TimeEntryDto(null, this.hours, this.minutes);
+    if ((this.hours > 0 || this.minutes > 0) && this.date) {
+      let timeEntry = new TimeEntryDto(null, this.hours, this.minutes, this.date);
 
       this.categoryService.saveTimeEntry(timeEntry, this.data.categoryId).subscribe({
         next: (savedItem) => {
--- src/models/TimeEntryDto.ts
+++ src/models/TimeEntryDto.ts
@@ -2,10 +2,12 @@ export class TimeEntryDto {
     id: string | null;
     hours: number;
     minutes: number;
+    date: Date;
 
-    constructor(id: string | null, hours: number, minutes: number){
+    constructor(id: string | null, hours: number, minutes: number, date: Date){
         this.id = id;
         this.hours = hours;
         this.minutes = minutes;
+        this.date = date;
     }
 }
